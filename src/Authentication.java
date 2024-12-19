d0985e413311dc9a00bbf19a9a714eab0011af35
Przemus14s <przemekszynal5@gmail.com>
2024-12-19 18:24:31 +0100

    Dodanie nowej funkcji: Możliwość zarejestrowania się oraz logowania

---
+++ src/Authentication.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Authentication {
+    private List<User> users;
+
+    public Authentication() {
+        users = new ArrayList<>();
+    }
+
+    public void register(String username, String password, String role) {
+        users.add(new User(username, password, role));
+    }
+
+    public User login(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
