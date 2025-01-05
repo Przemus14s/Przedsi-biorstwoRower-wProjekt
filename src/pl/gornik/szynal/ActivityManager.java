d2d209154d27fb441b8d3da469e13d5dea54ad6f
Przemus14s <przemekszynal5@gmail.com>
2025-01-05 18:29:32 +0100

    Utworzenie funkcji która umożliwia na dodawanie opisu aktywności z poziomu Managera

---
+++ src/pl/gornik/szynal/ActivityManager.java
@@ -0,0 +1,24 @@
+package pl.gornik.szynal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityManager {
+    private List<String> activities = new ArrayList<>();
+
+    public void addActivity(String activity) {
+        activities.add(activity);
+        System.out.println("Dodano aktywność: " + activity);
+    }
+
+    public void listActivities() {
+        if (activities.isEmpty()) {
+            System.out.println("Brak zapisanych aktywności.");
+        } else {
+            System.out.println("Lista aktywności:");
+            for (String activity : activities) {
+                System.out.println("- " + activity);
+            }
+        }
+    }
+}
--- src/pl/gornik/szynal/Main.java
+++ src/pl/gornik/szynal/Main.java
@@ -10,6 +10,7 @@ public class Main {
         CertificateManager certificateManager = new CertificateManager();
         OrderManager orderManager = new OrderManager();
         FeedbackManager feedbackManager = new FeedbackManager();
+        ActivityManager activityManager = new ActivityManager();
 
         while (true) {
             System.out.println("=== Fabryka Rowerów ===");
@@ -45,152 +46,140 @@ public class Main {
 
                     if ("manager".equals(loggedInUser.getRole())) {
                         while (true) {
-                            System.out.println("\nMenu Kierownika:");
-                            System.out.println("1. Zarządzaj certyfikatami");
-                            System.out.println("2. Zarządzaj magazynem");
-                            System.out.println("3. Zarządzaj zamówieniami");
-                            System.out.println("4. Wyloguj się");
+                            System.out.println("\n=== Menu Kierownika ===");
+                            System.out.println("1. Zarządzaj magazynem");
+                            System.out.println("2. Zarządzaj certyfikatami");
+                            System.out.println("3. Wyświetl zamówienia");
+                            System.out.println("4. Realizuj zamówienie");
+                            System.out.println("5. Dodaj aktywność");
+                            System.out.println("6. Wyświetl aktywności");
+                            System.out.println("7. Wyloguj się");
                             System.out.print("Wybierz opcję: ");
 
                             int managerChoice = scanner.nextInt();
                             scanner.nextLine();
 
                             if (managerChoice == 1) {
-                                while (true) {
-                                    System.out.println("\nMenu Certyfikatów:");
-                                    System.out.println("1. Dodaj certyfikat");
-                                    System.out.println("2. Wyświetl certyfikaty");
-                                    System.out.println("3. Wróć");
-                                    System.out.print("Wybierz opcję: ");
+                                System.out.println("1. Dodaj część");
+                                System.out.println("2. Wyświetl części");
+                                System.out.print("Wybierz opcję: ");
+                                int warehouseChoice = scanner.nextInt();
+                                scanner.nextLine();
 
-                                    int certChoice = scanner.nextInt();
-                                    scanner.nextLine();
-
-                                    if (certChoice == 1) {
-                                        System.out.print("Podaj nazwę certyfikatu: ");
-                                        String certificate = scanner.nextLine();
-                                        certificateManager.addCertificate(certificate);
-                                        System.out.println("Certyfikat został dodany.");
-                                    } else if (certChoice == 2) {
-                                        certificateManager.listCertificates();
-                                    } else if (certChoice == 3) {
-                                        break;
-                                    } else {
-                                        System.out.println("Nieprawidłowa opcja. Spróbuj ponownie.");
-                                    }
+                                if (warehouseChoice == 1) {
+                                    System.out.print("Podaj nazwę części do dodania: ");
+                                    String part = scanner.nextLine();
+                                    warehouse.addPart(part);
+                                    System.out.println("Dodano część do magazynu.");
+                                } else if (warehouseChoice == 2) {
+                                    warehouse.listParts();
                                 }
+
                             } else if (managerChoice == 2) {
-                                while (true) {
-                                    System.out.println("\nMenu Magazynu:");
-                                    System.out.println("1. Dodaj część");
-                                    System.out.println("2. Wyświetl części");
-                                    System.out.println("3. Wróć");
-                                    System.out.print("Wybierz opcję: ");
+                                System.out.println("1. Dodaj certyfikat");
+                                System.out.println("2. Wyświetl certyfikaty");
+                                System.out.print("Wybierz opcję: ");
+                                int certChoice = scanner.nextInt();
+                                scanner.nextLine();
 
-                                    int whChoice = scanner.nextInt();
-                                    scanner.nextLine();
-
-                                    if (whChoice == 1) {
-                                        System.out.print("Podaj nazwę części do dodania: ");
-                                        String part = scanner.nextLine();
-                                        warehouse.addPart(part);
-                                        System.out.println("Część została dodana.");
-                                    } else if (whChoice == 2) {
-                                        warehouse.listParts();
-                                    } else if (whChoice == 3) {
-                                        break;
-                                    } else {
-                                        System.out.println("Nieprawidłowa opcja. Spróbuj ponownie.");
-                                    }
+                                if (certChoice == 1) {
+                                    System.out.print("Podaj treść certyfikatu: ");
+                                    String certificate = scanner.nextLine();
+                                    certificateManager.addCertificate(certificate);
+                                    System.out.println("Dodano certyfikat.");
+                                } else if (certChoice == 2) {
+                                    certificateManager.listCertificates();
                                 }
+
                             } else if (managerChoice == 3) {
-                                while (true) {
-                                    System.out.println("\nMenu Zamówień:");
-                                    System.out.println("1. Wyświetl zamówienia");
-                                    System.out.println("2. Zrealizuj zamówienie");
-                                    System.out.println("3. Wróć");
-                                    System.out.print("Wybierz opcję: ");
+                                orderManager.listOrders();
 
-                                    int orderChoice = scanner.nextInt();
-                                    scanner.nextLine();
-
-                                    if (orderChoice == 1) {
-                                        orderManager.listOrders();
-                                    } else if (orderChoice == 2) {
-                                        System.out.print("Podaj numer zamówienia do realizacji: ");
-                                        int orderIndex = scanner.nextInt();
-                                        scanner.nextLine();
-                                        orderManager.fulfillOrder(orderIndex, warehouse);
-                                    } else if (orderChoice == 3) {
-                                        break;
-                                    } else {
-                                        System.out.println("Nieprawidłowa opcja. Spróbuj ponownie.");
-                                    }
-                                }
                             } else if (managerChoice == 4) {
+                                System.out.print("Podaj numer zamówienia do realizacji: ");
+                                int orderIndex = scanner.nextInt();
+                                scanner.nextLine();
+                                orderManager.fulfillOrder(orderIndex, warehouse);
+
+                            } else if (managerChoice == 5) {
+                                System.out.print("Podaj opis aktywności: ");
+                                String activity = scanner.nextLine();
+                                activityManager.addActivity(activity);
+                                System.out.println("Dodano aktywność.");
+
+                            } else if (managerChoice == 6) {
+                                activityManager.listActivities();
+
+                            } else if (managerChoice == 7) {
                                 System.out.println("Wylogowano.");
                                 break;
                             } else {
-                                System.out.println("Nieprawidłowa opcja. Spróbuj ponownie.");
+                                System.out.println("Nieprawidłowa opcja.");
                             }
                         }
+
                     } else if ("employee".equals(loggedInUser.getRole())) {
                         while (true) {
-                            System.out.println("\nMenu Pracownika:");
+                            System.out.println("\n=== Menu Pracownika ===");
                             System.out.println("1. Złóż zamówienie");
                             System.out.println("2. Wyświetl certyfikaty");
-                            System.out.println("3. Wystaw opinię i ocenę");
-                            System.out.println("4. Wyloguj się");
+                            System.out.println("3. Wyświetl opinie");
+                            System.out.println("4. Dodaj opinię");
+                            System.out.println("5. Wyloguj się");
                             System.out.print("Wybierz opcję: ");
 
-                            int empChoice = scanner.nextInt();
+                            int employeeChoice = scanner.nextInt();
                             scanner.nextLine();
 
-                            if (empChoice == 1) {
+                            if (employeeChoice == 1) {
                                 Map<String, String> order = new HashMap<>();
-                                System.out.print("Podaj rodzaj ramy: ");
+                                System.out.print("Podaj typ ramy: ");
                                 order.put("Rama", scanner.nextLine());
                                 System.out.print("Podaj rozmiar kół: ");
                                 order.put("Koła", scanner.nextLine());
-                                System.out.print("Podaj rodzaj pedałów: ");
+                                System.out.print("Podaj typ pedałów: ");
                                 order.put("Pedały", scanner.nextLine());
-                                System.out.print("Podaj rodzaj kierownicy: ");
+                                System.out.print("Podaj typ kierownicy: ");
                                 order.put("Kierownica", scanner.nextLine());
-                                System.out.print("Dodaj dzwonek (tak/nie): ");
+                                System.out.print("Podaj typ dzwonka: ");
                                 order.put("Dzwonek", scanner.nextLine());
-                                System.out.print("Dodaj światła (tak/nie): ");
+                                System.out.print("Podaj typ świateł: ");
                                 order.put("Światła", scanner.nextLine());
 
                                 orderManager.addOrder(order);
-                                System.out.println("Zamówienie zostało złożone.");
-                            } else if (empChoice == 2) {
+                                System.out.println("Złożono zamówienie.");
+
+                            } else if (employeeChoice == 2) {
                                 certificateManager.listCertificates();
-                            } else if (empChoice == 3) {
-                                System.out.print("Podaj ocenę roweru (1-5): ");
+
+                            } else if (employeeChoice == 3) {
+                                feedbackManager.listFeedbacks();
+
+                            } else if (employeeChoice == 4) {
+                                System.out.print("Podaj ocenę (1-5): ");
                                 int rating = scanner.nextInt();
                                 scanner.nextLine();
                                 System.out.print("Podaj komentarz: ");
                                 String comment = scanner.nextLine();
-
                                 feedbackManager.addFeedback(rating, comment);
-                                System.out.println("Opinia została dodana.");
-                            } else if (empChoice == 4) {
+                                System.out.println("Dodano opinię.");
+
+                            } else if (employeeChoice == 5) {
                                 System.out.println("Wylogowano.");
                                 break;
                             } else {
-                                System.out.println("Nieprawidłowa opcja. Spróbuj ponownie.");
+                                System.out.println("Nieprawidłowa opcja.");
                             }
                         }
                     }
                 } else {
-                    System.out.println("Nieprawidłowe dane logowania. Spróbuj ponownie.");
+                    System.out.println("Nieprawidłowa nazwa użytkownika lub hasło.");
                 }
 
             } else if (choice == 3) {
                 System.out.println("Do widzenia!");
                 break;
             } else {
-                System.out.println("Nieprawidłowa opcja. Spróbuj ponownie.");
+                System.out.println("Nieprawidłowa opcja.");
             }
         }
 
