fd81975630fac81cdec3bfe3c95d1e489b0c0ae7
Przemus14s <przemekszynal5@gmail.com>
2025-01-07 19:02:20 +0100

    Przeniesienie skryptu z klasy Main do klasy App tak aby klasa Main była tylko i wyłącznie klasą wykonywawczą

---
+++ src/pl/gornik/szynal/App.java
@@ -0,0 +1,218 @@
+package pl.gornik.szynal;
+
+import pl.gornik.szynal.management.*;
+import pl.gornik.szynal.users.Authentication;
+import pl.gornik.szynal.users.User;
+
+import java.util.*;
+
+public class App {
+    private Scanner scanner = new Scanner(System.in);
+    private Authentication auth = new Authentication();
+    private Warehouse warehouse = new Warehouse();
+    private CertificateManager certificateManager = new CertificateManager();
+    private OrderManager orderManager = new OrderManager();
+    private FeedbackManager feedbackManager = new FeedbackManager();
+    private ActivityManager activityManager = new ActivityManager();
+
+    public void run() {
+        while (true) {
+            System.out.println("=== Fabryka Rowerów ===");
+            System.out.println("1. Zarejestruj się");
+            System.out.println("2. Zaloguj się");
+            System.out.println("3. Wyjdź");
+            System.out.print("Wybierz opcję: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice == 1) {
+                register();
+            } else if (choice == 2) {
+                login();
+            } else if (choice == 3) {
+                System.out.println("Do widzenia!");
+                break;
+            } else {
+                System.out.println("Nieprawidłowa opcja.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private void register() {
+        System.out.print("Podaj nazwę użytkownika: ");
+        String username = scanner.nextLine();
+        System.out.print("Podaj hasło: ");
+        String password = scanner.nextLine();
+        System.out.print("Podaj rolę (employee/manager): ");
+        String role = scanner.nextLine();
+
+        auth.register(username, password, role);
+        System.out.println("Rejestracja zakończona sukcesem!");
+    }
+
+    private void login() {
+        System.out.print("Nazwa użytkownika: ");
+        String username = scanner.nextLine();
+        System.out.print("Hasło: ");
+        String password = scanner.nextLine();
+
+        User loggedInUser = auth.login(username, password);
+
+        if (loggedInUser != null) {
+            System.out.println("Zalogowano pomyślnie! Witaj, " + loggedInUser.getRole() + " " + loggedInUser.getUsername() + ".");
+            if ("manager".equals(loggedInUser.getRole())) {
+                managerMenu();
+            } else if ("employee".equals(loggedInUser.getRole())) {
+                employeeMenu();
+            }
+        } else {
+            System.out.println("Nieprawidłowa nazwa użytkownika lub hasło.");
+        }
+    }
+
+    private void managerMenu() {
+        while (true) {
+            System.out.println("\n=== Menu Kierownika ===");
+            System.out.println("1. Zarządzaj magazynem");
+            System.out.println("2. Zarządzaj certyfikatami");
+            System.out.println("3. Wyświetl zamówienia");
+            System.out.println("4. Realizuj zamówienie");
+            System.out.println("5. Dodaj aktywność");
+            System.out.println("6. Wyświetl aktywności");
+            System.out.println("7. Wyloguj się");
+            System.out.print("Wybierz opcję: ");
+
+            int managerChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (managerChoice == 1) {
+                warehouseMenu();
+            } else if (managerChoice == 2) {
+                certificateMenu();
+            } else if (managerChoice == 3) {
+                orderManager.listOrders();
+            } else if (managerChoice == 4) {
+                fulfillOrder();
+            } else if (managerChoice == 5) {
+                addActivity();
+            } else if (managerChoice == 6) {
+                activityManager.listActivities();
+            } else if (managerChoice == 7) {
+                System.out.println("Wylogowano.");
+                break;
+            } else {
+                System.out.println("Nieprawidłowa opcja.");
+            }
+        }
+    }
+
+    private void warehouseMenu() {
+        System.out.println("1. Dodaj część");
+        System.out.println("2. Wyświetl części");
+        System.out.print("Wybierz opcję: ");
+        int warehouseChoice = scanner.nextInt();
+        scanner.nextLine();
+
+        if (warehouseChoice == 1) {
+            System.out.print("Podaj nazwę części do dodania: ");
+            String part = scanner.nextLine();
+            warehouse.addPart(part);
+            System.out.println("Dodano część do magazynu.");
+        } else if (warehouseChoice == 2) {
+            warehouse.listParts();
+        }
+    }
+
+    private void certificateMenu() {
+        System.out.println("1. Dodaj certyfikat");
+        System.out.println("2. Wyświetl certyfikaty");
+        System.out.print("Wybierz opcję: ");
+        int certChoice = scanner.nextInt();
+        scanner.nextLine();
+
+        if (certChoice == 1) {
+            System.out.print("Podaj treść certyfikatu: ");
+            String certificate = scanner.nextLine();
+            certificateManager.addCertificate(certificate);
+            System.out.println("Dodano certyfikat.");
+        } else if (certChoice == 2) {
+            certificateManager.listCertificates();
+        }
+    }
+
+    private void fulfillOrder() {
+        System.out.print("Podaj numer zamówienia do realizacji: ");
+        int orderIndex = scanner.nextInt();
+        scanner.nextLine();
+        orderManager.fulfillOrder(orderIndex, warehouse);
+    }
+
+    private void addActivity() {
+        System.out.print("Podaj opis aktywności: ");
+        String activity = scanner.nextLine();
+        activityManager.addActivity(activity);
+        System.out.println("Dodano aktywność.");
+    }
+
+    private void employeeMenu() {
+        while (true) {
+            System.out.println("\n=== Menu Pracownika ===");
+            System.out.println("1. Złóż zamówienie");
+            System.out.println("2. Wyświetl certyfikaty");
+            System.out.println("3. Wyświetl opinie");
+            System.out.println("4. Dodaj opinię");
+            System.out.println("5. Wyloguj się");
+            System.out.print("Wybierz opcję: ");
+
+            int employeeChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (employeeChoice == 1) {
+                placeOrder();
+            } else if (employeeChoice == 2) {
+                certificateManager.listCertificates();
+            } else if (employeeChoice == 3) {
+                feedbackManager.listFeedbacks();
+            } else if (employeeChoice == 4) {
+                addFeedback();
+            } else if (employeeChoice == 5) {
+                System.out.println("Wylogowano.");
+                break;
+            } else {
+                System.out.println("Nieprawidłowa opcja.");
+            }
+        }
+    }
+
+    private void placeOrder() {
+        Map<String, String> order = new HashMap<>();
+        System.out.print("Podaj typ ramy: ");
+        order.put("Rama", scanner.nextLine());
+        System.out.print("Podaj rozmiar kół: ");
+        order.put("Koła", scanner.nextLine());
+        System.out.print("Podaj typ pedałów: ");
+        order.put("Pedały", scanner.nextLine());
+        System.out.print("Podaj typ kierownicy: ");
+        order.put("Kierownica", scanner.nextLine());
+        System.out.print("Podaj typ dzwonka: ");
+        order.put("Dzwonek", scanner.nextLine());
+        System.out.print("Podaj typ świateł: ");
+        order.put("Światła", scanner.nextLine());
+
+        orderManager.addOrder(order);
+        System.out.println("Złożono zamówienie.");
+    }
+
+    private void addFeedback() {
+        System.out.print("Podaj ocenę (1-5): ");
+        int rating = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Podaj komentarz: ");
+        String comment = scanner.nextLine();
+        feedbackManager.addFeedback(rating, comment);
+        System.out.println("Dodano opinię.");
+    }
+}
